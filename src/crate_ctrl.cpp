1b6a9474e979faad6d81efa4f31582490121f8df
Adam Frank <adam@adminsports.com>
2022-04-11 22:28:50 -0400

    crate root isolation finished

--- src/crate_ctrl.cpp
+++ src/crate_ctrl.cpp
@@ -5,7 +5,15 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/mount.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
 #include <sched.h>
+
+
+#define pivot_root(new_root, put_old) syscall(SYS_pivot_root, new_root, put_old)
+#define CRATE_FS_DIR "fs"
+#define CRATE_FS_OLD_ROOT_DIR "./host_root"
 
 int crate_proc_run(const char * exec_path, char * const * exec_argv) {
 
@@ -48,13 +56,64 @@ int crate_proc_run(const char * exec_path, char * const * exec_argv) {
 
 void proc_inherit_crate_spec() {
 
-    // create a new unix time share
-    if (unshare(CLONE_NEWUTS) == -1) {
+    // create a new unix time share and mount share
+    if (unshare(CLONE_NEWUTS | CLONE_NEWNS) == -1) {
         PrettyPrint::log_error("when running unshare: %s", strerror(errno));
         perror("unshare");
         _exit(HBRC_EXIT_CODE_UTSERR);
     }
 
     sethostname("testcontainer", 14);
+
+    if (mount("none", "/", NULL, MS_REC | MS_PRIVATE, NULL) == -1) {
+        PrettyPrint::log_error("unable to mount / to none", true);
+        _exit(HBRC_EXIT_CODE_UNKNOWN);
+    }
+
+    // mount the crate file system
+    if (mount(CRATE_FS_DIR, CRATE_FS_DIR, NULL, MS_BIND | MS_PRIVATE | MS_REC, NULL) == -1) {
+        PrettyPrint::log_error("unable to mount crate file system", true);
+        _exit(HBRC_EXIT_CODE_UNKNOWN);
+    }
+
+    // change directory to file system
+    if (chdir(CRATE_FS_DIR) == -1) {
+        PrettyPrint::log_error("crate file system does not exist, or insufficient access", true);
+        _exit(HBRC_EXIT_CODE_UNKNOWN);
+    }
+
+    // create directory for old root for root pivoting
+    if (mkdir(CRATE_FS_OLD_ROOT_DIR, 0755) == -1) {
+        PrettyPrint::log_error("unable to create old root directory", true);
+        _exit(HBRC_EXIT_CODE_UNKNOWN);
+    }
+    
+    // pivot root to switch the root directory for the containerized process
+    if (syscall(SYS_pivot_root, ".", CRATE_FS_OLD_ROOT_DIR) == -1) {
+        PrettyPrint::log_error("unable to pivot root to crate file system", true);
+        perror("pivot_root");
+        printf("%i\n", errno);
+        _exit(HBRC_EXIT_CODE_UTSERR);
+    }
+
+    // change directories to new root
+    if (chdir("/") == -1) {
+        PrettyPrint::log_error("unable to switch to new root file system", true);
+        _exit(HBRC_EXIT_CODE_UTSERR);
+    }
+
+    // unmount the old_root to prevent host machine access
+    if (umount2(CRATE_FS_OLD_ROOT_DIR, MNT_DETACH)) {
+        PrettyPrint::log_error("unable to unmount host root from containerized process", true);
+        _exit(HBRC_EXIT_CODE_UTSERR);
+    }
+
+    // remove the old_root directory
+    if (rmdir(CRATE_FS_OLD_ROOT_DIR) == -1) {
+        PrettyPrint::log_error("unable to rm host root directory from containerized process", true);
+        _exit(HBRC_EXIT_CODE_UTSERR);
+    }
+
+
 
 }
